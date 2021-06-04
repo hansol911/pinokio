311eb7e808b8db6c5db0bb7cda0171fba1b2e0be
hansol911 <hansol@micehub.com>
2021-06-05 00:47:17 +0900

    category / exception 수정

--- src/main/java/com/pinokio/pino/PinoApplication.java
+++ src/main/java/com/pinokio/pino/PinoApplication.java
@@ -4,12 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@EnableJpaRepositories
 @SpringBootApplication
+@EnableJpaAuditing
 public class PinoApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/pinokio/pino/api/category/CategoryRepo.java
+++ src/main/java/com/pinokio/pino/api/category/CategoryRepo.java
@@ -1,7 +1,6 @@
 package com.pinokio.pino.api.category;
 
 import com.pinokio.pino.api.model.Category;
-import org.apache.ibatis.annotations.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,6 +8,6 @@ import java.util.List;
 
 @Repository
 public interface CategoryRepo extends JpaRepository<Category, Long> {
-    List<Category> findByWood_WoodNum(@Param(value = "wood_num") Long woodNum);
+    List<Category> findByWood_WoodNum(Long woodNum);
 }
 
